50a836defe9bfee80c9195bb22403e8b8c15d69d
TsujioRyosuke(Let's Note) <tsujioryo@otsukaele.jp>
2020-02-06 18:09:42 +0900

    試薬分注クラス追加

---
+++ src/UnitReagentDisp.h
@@ -0,0 +1,14 @@
+// UnitReagentDisp.h: UnitReagentDisp 試薬分注ユニットクラス
+//
+//////////////////////////////////////////////////////////////////////
+
+/***********************************************************************/
+//! @class UnitBFTable     @brief BFテーブルクラス
+class UnitReagentDisp public UnitBase:
+{
+private:
+	
+public:
+				UnitReagentDisp							(LPTSTR name, DWORD stackSize);		//!< コンストラクタ
+	virtual		~UnitReagentDisp							();									//!< デストラクタ
+};
